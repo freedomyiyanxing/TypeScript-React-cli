2c4f9bdbacbf263f346303fc5cc92d53ab4717e1
freedom.yi <851989962freedom.yi@gmail.com>
2019-12-10 15:20:58 +0800

    测试2

--- src/common/material-ui/links.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Link } from '@material-ui/core';
-import { Link as RouterLink } from 'react-router-dom';
-
-const LinkRouter: React.FC<any> = (props) => (
-  <Link {...props} component={RouterLink} />
-);
-
-export default LinkRouter;
